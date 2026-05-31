5608f4f743e29721600191f005ac3da4c07624d2
glennmueller9-design <glenn.mueller9@icloud.com>
2026-05-31 15:28:08 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('purzelhuus-v1').then(c=>c.addAll(['./','./index.html','./menus.js','./manifest.webmanifest','./icon.svg'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
